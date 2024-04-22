65bb61faa501eef2cdcff2d4a63a473ef88155e6
raphael-nibihaka <raphaelnibishaka@gmail.com>
2024-04-22 13:16:18 +0400

    un commenting bet for first user

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ screen = Screen()
 screen.title("Raphael Tutle racing")
 screen.setup(width=500, height=400)
 
-# bet1 = screen.textinput("First User Enter your bet", "which win between Red, Blue, Green and Yellow Turtles Enter your names and bet ie # ma:black").lower()
+bet1 = screen.textinput("First User Enter your bet", "which win between Red, Blue, Green and Yellow Turtles Enter your names and bet ie # ma:black").lower()
 bet2 = screen.textinput("Second User Enter your bet", "which win between Red, Blue, Green and Yellow Turtles Enter your names and bet ie # ma:black").lower()
 bet3 = screen.textinput("Third User Enter your bet", "which win between Red, Blue, Green and Yellow Turtles Enter your names and bet ie # ma:black").lower()
 bet4 = screen.textinput("Fourth User Enter your bet", "which win between Red, Blue, Green and Yellow Turtles Enter your names and bet ie # ma:black").lower()
-# bet1_info = bet1.split(":")
+bet1_info = bet1.split(":")
 bet2_info = bet2.split(":")
 bet3_info = bet3.split(":")
 bet4_info = bet4.split(":")
-# name1 = bet1_info[0].title()
+name1 = bet1_info[0].title()
 name2 = bet2_info[0].title()
 name3 = bet3_info[0].title()
 name4 = bet4_info[0].title()
 
-# betting1 = bet1_info[1]
+betting1 = bet1_info[1]
 betting2 = bet2_info[1]
 betting3 = bet3_info[1]
 betting4 = bet4_info[1]
@@ -66,8 +66,8 @@ while race:
             winning_color = (racer.pencolor())
 
             race = False
-            # if winning_color == betting1:
-            #     print(f"The winner of the race is: {winning_color} so {name1} wins")
+            if winning_color == betting1:
+                print(f"The winner of the race is: {winning_color} so {name1} wins")
             if winning_color == betting2:
                 print(f"The winner of the race is: {winning_color} so {name2} wins")
             elif winning_color == betting3:
